c430f14db0f808e340b18829d50ae4b3a35298c1
Reese Parker <parker.rhys@outlook.com>
2022-03-20 17:04:19 -0400

    Added MUI components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+import Drawer from "@mui/material/Drawer";
+
+import Navbar from "./Navbar";
+import Menu from "./Menu"
+import useToggleState from "./hooks/useToggleState";
+
 function App() {
-  return <div className="App">innit</div>;
+
+  const [isDrawerOpen, toggleDrawer] = useToggleState(false);
+
+
+  return (
+    <div className="App">
+      <Navbar handleOpenDrawer={toggleDrawer} />
+      <Drawer
+        anchor={"left"}
+        open={isDrawerOpen}
+        onClose={toggleDrawer}
+        PaperProps={{
+          sx: {width: "150px"}
+        }}
+        elevation={1}
+      >
+        <Menu handleCloseDrawer={toggleDrawer} />
+      </Drawer>
+    </div>
+  );
 }
 
 export default App;
