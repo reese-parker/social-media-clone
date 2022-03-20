cbdab5d7d58571913463a054bd8af1129a9c5b8b
Reese Parker <parker.rhys@outlook.com>
2022-03-20 17:57:09 -0400

    Created Feed component

---
+++ src/Feed.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import Grid from "@mui/material/Grid";
+
+import Post from "./Post";
+
+const styles = {
+  PostContainer: {
+    width: "96%",
+  },
+};
+
+export default function Feed() {
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={1}
+    >
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+      <Grid sx={styles.PostContainer} item xs={12}>
+        <Post />
+      </Grid>
+   
+    </Grid>
+  );
+}
