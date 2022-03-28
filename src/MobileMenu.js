cfaf74fa90766e0d1b0b075ecbb2a9bd04826c57
Reese Parker <parker.rhys@outlook.com>
2022-03-28 15:27:24 -0400

    Added menus for desktop and mobile

---
+++ src/MobileMenu.js
@@ -0,0 +1,138 @@
+import React, { useContext } from "react";
+
+import { useNavigate } from "react-router-dom";
+import { auth } from "./firebase.js";
+import { signOut } from "firebase/auth";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Typography } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FaceIcon from "@mui/icons-material/Face";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
+import LoginIcon from "@mui/icons-material/Login";
+import HomeIcon from "@mui/icons-material/Home";
+
+import {
+  ActiveUserContext,
+  ActiveUserDispatchContext,
+} from "./contexts/ActiveUserContext";
+import styles from "./styles/MenuStyles.js";
+
+export default function MobileMenu(props) {
+  const { handleCloseDrawer, handleOpenSnackbar } = props;
+  const activeUser = useContext(ActiveUserContext);
+  const setActiveUser = useContext(ActiveUserDispatchContext);
+
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    await signOut(auth);
+    setActiveUser(null);
+    handleCloseDrawer();
+    navigate("/signin");
+    handleOpenSnackbar("Signed out");
+  };
+
+  return (
+    <Box sx={styles.container}>
+      {
+        // Menu header for signed in users
+        activeUser && (
+          <ListItem divider sx={styles.userInfoContainer}>
+            <ListItemIcon sx={styles.userInfoContainer}>
+              {activeUser.displayName === "Demo" ? (
+                <AccountCircleIcon color="primary" />
+              ) : (
+                <FaceIcon color="primary" />
+              )}
+              <Typography sx={styles.displayName}>
+                {activeUser.displayName}
+              </Typography>
+            </ListItemIcon>
+          </ListItem>
+        )
+      }
+
+      <List>
+        <ListItem
+          sx={styles.ListItem}
+          button
+          onClick={() => {
+            handleCloseDrawer();
+            navigate("/feed");
+          }}
+        >
+          <ListItemIcon>
+            <HomeIcon color="primary" />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+
+        {
+          // Conditional list items based on activeUser
+          activeUser === null ? (
+            <>
+              <ListItem
+                sx={styles.ListItem}
+                button
+                onClick={() => {
+                  handleCloseDrawer();
+                  navigate("/signin");
+                }}
+              >
+                <ListItemIcon>
+                  <LoginIcon color="primary" />
+                </ListItemIcon>
+                <ListItemText primary="Sign in" />
+              </ListItem>
+              <ListItem
+                sx={styles.ListItem}
+                button
+                onClick={() => {
+                  handleCloseDrawer();
+                  navigate("/signup");
+                }}
+              >
+                <ListItemIcon>
+                  <AccountCircleIcon color="primary" />
+                </ListItemIcon>
+                <ListItemText primary="Sign up" />
+              </ListItem>
+            </>
+          ) : (
+            <>
+              <ListItem
+                sx={styles.ListItem}
+                button
+                onClick={() => {
+                  handleCloseDrawer();
+                  navigate("/accountsettings");
+                }}
+              >
+                <ListItemIcon>
+                  <SettingsIcon color="primary" />
+                </ListItemIcon>
+                <ListItemText primary="Settings"></ListItemText>
+              </ListItem>
+              <ListItem
+                divider
+                sx={styles.ListItem}
+                button
+                onClick={handleSignOut}
+              >
+                <ListItemIcon>
+                  <LogoutIcon color="primary" />
+                </ListItemIcon>
+                <ListItemText primary="Sign out" />
+              </ListItem>
+            </>
+          )
+        }
+      </List>
+    </Box>
+  );
+}
