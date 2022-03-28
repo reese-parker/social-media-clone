d7a1459f2ef115c3c583d5ca2ee92843db108995
Reese Parker <parker.rhys@outlook.com>
2022-03-27 21:37:11 -0400

    Updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,15 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAVOwIetMkirgzjVYYdKAT19C_lMUBS5_s",
-  authDomain: "social-media-clone-277a6.firebaseapp.com",
-  projectId: "social-media-clone-277a6",
-  storageBucket: "social-media-clone-277a6.appspot.com",
-  messagingSenderId: "413486263040",
-  appId: "1:413486263040:web:4ab650ab01f08ec8a361e5",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_I,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
 
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
