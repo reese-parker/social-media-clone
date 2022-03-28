ebd0d621fa7cb48d810b6dc41bfe4eb9555423fb
Reese Parker <parker.rhys@outlook.com>
2022-03-28 15:27:40 -0400

    Created welcome message for demo users

---
+++ src/WelcomeMessage.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+
+export default function AlertDialog() {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You're currently viewing the demo version of this app. You can either use the demo account from the sign in page or create your own. Posts won't be saved to servers and certain account features are disabled.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>
+            Continue
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
